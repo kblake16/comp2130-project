1809528d398b7daf18ce3f11bfa94ad99b1c4120
kblake16 <kayla.blake16@gmail.com>
2021-03-24 21:14:26 -0500

    Edit Server

--- server.c
+++ server.c
@@ -14,8 +14,8 @@ Name            ID no.*/
 #define BUF_SIZE	1024
 #define LISTEN_PORT	60000
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[])
+{
     int	sock_listen,sock_recv;
     struct sockaddr_in	my_addr,recv_addr;
     int i,addr_size,bytes_received;
@@ -23,44 +23,89 @@ int main(int argc, char *argv[]){
     struct sockaddr	remote_addr;
     int	recv_msg_size;
     char buf[BUF_SIZE];
+    int const NUM_RANGE = 10;
+    int k,j;
 
+    char * grid[NUM_RANGE][NUM_RANGE];
 
-            /* create socket for listening */
+    void spreadSheet()
+    {
+        int num = 1;
+        char * const NLINE = "    A    B    C    D    E    F    G    H";
+        char * const HLINE = "  +----+----+----+----+----+----+----+----+";
+        char * const VLINE = num + "  |    |    |    |    |    |    |    |    |";
+
+        printf("%s\n",NLINE);
+        printf("%s\n",HLINE);
+        for (j = 0; j < NUM_RANGE; j++)
+        {  
+            printf("%s\n",VLINE);
+            printf("%d ",j+1);
+            for (k = 0; k < NUM_RANGE; k++)
+            {  if(strcmp(grid[k][j],"   ")==0)
+                {
+                    printf("| %s",grid[k][j]);
+                }
+                else
+                {
+                    printf("| %s  ",grid[k][j]);
+                }
+            }
+            printf("%s","|");
+            printf("\n");
+            printf("%s\n",VLINE);
+            printf("%s\n",HLINE);
+        }
+        return;
+    }
+
+    printf("Spreadsheet Server\n");
+
+    /* create socket for listening */
     sock_listen=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock_listen < 0){
+    if (sock_listen < 0)
+    {
         printf("socket() failed\n");
         exit(0);
     }
-        /* make local address structure */
+
+    printf("Socket Created\n");
+
+    /* make local address structure */
     memset(&my_addr, 0, sizeof (my_addr));	/* zero out structure */
     my_addr.sin_family = AF_INET;	/* address family */
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);  /* current machine IP */
     my_addr.sin_port = htons((unsigned short)LISTEN_PORT);
 
-        /* bind socket to the local address */
+     /* bind socket to the local address */
     i=bind(sock_listen, (struct sockaddr *) &my_addr, sizeof (my_addr));
-    if (i < 0){
+    if (i < 0)
+    {
         printf("bind() failed\n");
         exit(0);
     }
     
-    
-        /* listen ... */
-    
+    printf("Socket Binded\n");
+
+    /* listen ... */
     i=listen(sock_listen, 5);
     if (i < 0){
         printf("listen() failed\n");
         exit(0);
     }
-     
- 
-        /* get new socket to receive data on */
-        /* It extracts the first connection request from the  */
-        /* listening socket  */
+    
+    printf("Socket now listening...\n");
+
+    /* get new socket to receive data on */
+    /* It extracts the first connection request from the  */
+    /* listening socket  */
     addr_size=sizeof(recv_addr);
     sock_recv=accept(sock_listen, (struct sockaddr *) &recv_addr, &addr_size);
 
+    printf("Connected\n");
+
     while (1){
+        spreadSheet();
         bytes_received=recv(sock_recv,buf,BUF_SIZE,0);
         buf[bytes_received]=0;
         printf("Received: %s\n",buf);
